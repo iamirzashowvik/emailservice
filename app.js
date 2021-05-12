f2d3d4f3d1a1bbe120094b8282ca56c88586831d
iamirzashowvik <showvikmirza@gmail.com>
2021-05-12 06:26:38 +0600

    SS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const https = require("https");
 const { url } = require("inspector");
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }));
